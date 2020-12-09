01f467718d4df3ea00e1b8884409eb9ef7b03f0e
Oxxy-k <70562469+Oxxy-k@users.noreply.github.com>
2020-12-09 10:26:16 +0300

    Update genre-bar.js

--- src/components/genre-bar/genre-bar.js
+++ src/components/genre-bar/genre-bar.js
@@ -2,20 +2,16 @@ import "./genre-bar.css";
 import React, { useState } from "react";
 
 const GenreBar = ({ Genre, onChangeFilterGenre }) => {
-  const [chooseGenre, setChooseGenre] = useState({
-    Genre: Genre,
-    filter: true,
-  });
+  const [chooseGenre, setChooseGenre] = useState({ Genre, filter: true });
   return (
     <div className="col-md">
       <label className="form-check" key={Genre}>
         <input
           className="form-check-input"
           type="checkbox"
-          id="materialChecked2"
           checked={chooseGenre.filter}
           onClick={() => {
-            setChooseGenre({ ...chooseGenre, filter: !chooseGenre.filter });
+            setChooseGenre({ Genre, filter: !chooseGenre.filter });
             onChangeFilterGenre(chooseGenre);
           }}
         />

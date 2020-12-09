ec131f8e53b5ce64343fe17a802d6837e98867db
Oxxy-k <70562469+Oxxy-k@users.noreply.github.com>
2020-12-09 14:22:15 +0300

    Update looking-for-search.js

--- src/components/looking-for-search/looking-for-search.js
+++ src/components/looking-for-search/looking-for-search.js
@@ -1,10 +1,14 @@
 import "./looking-for-search.css";
 import React from "react";
 
-const LookingForSearch = () => {
+const LookingForSearch = ({ errorMessage }) => {
   return (
     <div className="empty-screen">
-      <h2 className="enjoy">Enjoy watching films with us!</h2>
+      <h2 className="enjoy">
+        {errorMessage
+          ? "Sorry, your " + errorMessage.toLowerCase()
+          : "Enjoy watching films with us!"}
+      </h2>
       <img
         className="screen-picture"
         src="http://bizness-master.com/wp-content/uploads/2016/08/monitor-1.png"

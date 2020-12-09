d7c1e6ecf63da499b6e94e4ee86534b2cd7007d6
Oxxy-k <70562469+Oxxy-k@users.noreply.github.com>
2020-12-09 14:27:46 +0300

    Update spinner.js

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div class="loadingio-spinner-spin-f6v98fuxj1h">
-      <div class="ldio-ng3c4dxx8td">
+    <div className="loadingio-spinner-spin-f6v98fuxj1h">
+      <div className="ldio-ng3c4dxx8td">
         <div>
           <div></div>
         </div>

8a5ba277a72e443b4ab6d277f044d0a12bc9ae27
Oxxy-k <70562469+Oxxy-k@users.noreply.github.com>
2020-12-10 17:08:34 +0300

    refactoring getUniqueGenre

--- src/services/get-unique-genre.js
+++ src/services/get-unique-genre.js
@@ -1,16 +1,6 @@
 const getUniqueGenre = (arr) => {
-  let result = [];
-  let keyOfObj = [];
-  for (let i = 0; i < arr.length; i++) {
-    const { Genre } = arr[i];
-    keyOfObj.push(Genre);
-  }
-  for (let str of keyOfObj) {
-    if (!result.includes(str)) {
-      result.push(str);
-    }
-  }
-  return result;
+  const Genres = Array.from(arr, ({ Genre }) => Genre);
+  return [...new Set(Genres)];
 };
 
 export { getUniqueGenre };
